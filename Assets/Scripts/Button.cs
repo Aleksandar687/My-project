2825eed906e3e4f3664e690bd9d52bc990facb0b
Aleksandar687 <aleksandar.petrov-26z@mgberon.com>
2025-06-03 18:06:10 +0300

    Initial commit

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Button myButton;
+
+    void Start()
+    {
+        myButton.onClick.AddListener(HandleClick);
+    }
+
+    public void HandleClick()
+    {
+        Debug.Log("Button clicked!");
+    }
+}
